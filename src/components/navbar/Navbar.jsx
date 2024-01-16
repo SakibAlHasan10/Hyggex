c7441738827bd097ebaa1de744da6b7f7c34332f
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2024-01-16 18:12:25 +0600

    added title and favicon

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,61 @@
-import { NavLink } from 'react-router-dom';
-import logo from '../../assets/Component-40.png'
-const nav =<>
-<NavLink to={"/"}>
-    <li>Home</li>
-</NavLink>
-<NavLink to={"/flashcard"}>
-    <li>Flashcard</li>
-</NavLink>
-<NavLink to={"/contact"}>
-    <li>Contact</li>
-</NavLink>
-</>
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/Component-40.png";
+const nav = (
+  <>
+    <NavLink to={"/"}>
+      <li>Home</li>
+    </NavLink>
+    <NavLink to={"/flashcard"}>
+      <li>Flashcard</li>
+    </NavLink>
+    <NavLink to={"/contact"}>
+      <li>Contact</li>
+    </NavLink>
+  </>
+);
 const Navbar = () => {
-    return (
-        <div className="w-full bg-white flex items-center justify-between">
-            <div>
-                
-            <img src={logo} alt="hyggex-logo" />
+  return (
+    <div className="">
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
             </div>
-            <div>
-                <ul className='flex items-center gap-10 text-lg font-normal'>
-                    {nav}
-                </ul>
-            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {nav}
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl"><img src={logo} alt="" /></a>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 gap-6 text-lg font-normal">
+            {nav}
+                
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
