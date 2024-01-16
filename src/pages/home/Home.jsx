79a08c85de43c95cbfba0a01ba8333ec5161d494
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2024-01-16 19:02:30 +0600

    added page path

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,18 @@
-
+import { GoHome } from "react-icons/go";
+import { IoIosArrowForward } from "react-icons/io";
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <div className="flex items-center gap-2 text-[#696671] text-lg font-medium">
+            <GoHome className="w-[30px] h-[30px]"/> 
+            <IoIosArrowForward className="text-[#06286E]"/>
+            <span>Flashcard</span> 
+            <IoIosArrowForward className="text-[#06286E]"/>
+            <span>Mathematics</span>
+            <IoIosArrowForward className="text-[#06286E]"/>
+            <span className="text-lg font-semibold text-[#06286E]">Relation and Function</span>
+            </div>
+            <h2 className="text-[32px] font-bold bg-gradient-to-b text-center from-[#06286E] to-[#164EC0] inline-block text-transparent bg-clip-text">Relations and Functions ( Mathematics )</h2>
         </div>
     );
 };
--- src/pages/root/Root.jsx
+++ src/pages/root/Root.jsx
@@ -3,7 +3,7 @@ import Navbar from "../../components/navbar/Navbar";
 
 const Root = () => {
     return (
-        <div>
+        <div className="max-w-screen-xl mx-auto px-8">
             <Navbar/>
             <Outlet/>
         </div>
